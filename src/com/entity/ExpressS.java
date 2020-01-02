263e0b0e28126f70516946b1b7b99362b191bf56
winner158 <946447190@qq.com>
2020-01-02 09:50:57 +0800

    Initial commit

---
+++ src/com/entity/ExpressS.java
@@ -0,0 +1,9 @@
+package com.entity;
+
+/**快递站信息
+ * @Author: pfsun
+ * @Date: 2020-01-01 21:31
+ */
+public class ExpressS {
+    
+}
---
+++ src/com/entity/User.java
@@ -0,0 +1,11 @@
+package com.entity;
+
+/**用户信息
+ * @Author: pfsun
+ * @Date: 2020-01-01 21:31
+ */
+public class User {
+
+
+
+}
